2158a047246625aa05b337c1bfc9ee2e4ad0f8f7
venkata-sandeep-addala <168465210+venkata-sandeep-addala@users.noreply.github.com>
2026-01-24 17:35:42 +0530

    Complete Forntend

---
+++ Frontend/script.js
@@ -0,0 +1,7 @@
+function openChat() {
+    document.getElementById("chatbot-container").style.display = "flex";
+}
+
+function closeChat() {
+    document.getElementById("chatbot-container").style.display = "none";
+}
